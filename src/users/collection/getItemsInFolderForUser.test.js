18723a94be12a6e8e9cb7f8d71241047c5f89f4f
Ryan Parr <parrfolio@gmail.com>
2018-11-28 11:13:36 -0800

    Updated the changes you asked for.

--- src/users/collection/getItemsInFolderForUser.test.js
+++ src/users/collection/getItemsInFolderForUser.test.js
@@ -21,15 +21,12 @@ describe('Users - Collection - getItemsInFolderForUser', () => {
       requestLimitAuth: 20,
     })
   })
-  it('should get items in a folder in a user\'s collection from its username and a `folderId`', async () => {
 
-    const data = await client.getItemsInFolderForUser( username, folderId, pagination: { perPage: 1 } )
+  it('should get items in a folder in a user\'s collection from its username and a `folderId`', async () => {
+    const data = await client.getItemsInFolderForUser(username, folderId)
     data.should.be.an('object').and.have.property('releases')
-    data.should.be.an('object').and.have.property('pagination')
-    data.pagination.should.be.an('object').and.have.property('urls')
-    data.pagination.urls.should.be.an('object').and.have.all.keys('last', 'next')
-    parse(URL_REGEX.exec(data.pagination.urls.last)[1]).status.should.be.equal(status)
   })
+  
   it('should return a TypeError if no param', () => {
     client.getItemsInFolderForUser().catch(err => err.should.be.an.instanceOf(TypeError))
   })
@@ -42,7 +39,7 @@ describe('Users - Collection - getItemsInFolderForUser', () => {
   it('should accept sort', async () => {
     const by = sortFields[Math.floor(Math.random() * sortFields.length)]
     const order = sortOrders[Math.floor(Math.random() * sortOrders.length)]
-    const data = await client.getItemsInFolderForUser(username, folderId, { by, order })
+    const data = await client.getItemsInFolderForUser(username, folderId, {}, { by, order })
     data.should.be.an('object').and.have.property('pagination')
     data.pagination.should.be.an('object').and.have.property('urls')
     data.pagination.urls.should.be.an('object').and.have.all.keys('last', 'next')
@@ -53,10 +50,7 @@ describe('Users - Collection - getItemsInFolderForUser', () => {
   it('should accept pagination', async () => {
     const page = 1
     const perPage = 3
-    const data = await client.getItemsInFolderForUser({
-      username,
-      pagination: { page, perPage },
-    })
+    const data = await client.getItemsInFolderForUser(username, folderId, { page, perPage })
     data.should.be.an('object').and.have.property('pagination')
     data.pagination.should.be.an('object')
     data.pagination.should.have.property('per_page')
